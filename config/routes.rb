7dc3c5a51415300de986c45e3a447735d1128838
Md. Mazharul Islam <mithu.cste.30@gmail.com>
2012-10-19 12:08:37 +0600

    added a users controller and layout link

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
   root :to => "pages#home"
   match '/contact', :to => 'pages#contact'
   match '/help',    :to => 'pages#help'
-  match '/about',   :to => 'pages#about' 
+  match '/about',   :to => 'pages#about'
+  match '/signup',  :to => 'users#new' 
 
   #get "pages/home"
 
