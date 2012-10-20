c50aec7481f774ddcd3197b2861639b9cc6cca96
Md. Mazharul Islam <mithu.cste.30@gmail.com>
2012-10-20 23:50:28 +0600

    Added a stub User show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 SampleApp::Application.routes.draw do
+  resources :users
   get "users/new"
 
   root :to => "pages#home"
   match '/contact', :to => 'pages#contact'
   match '/help',    :to => 'pages#help'
   match '/about',   :to => 'pages#about'
-  match '/signup',  :to => 'users#new' 
+  match '/signup',  :to => 'users#new'
 
   #get "pages/home"
 
