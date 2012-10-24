73294696858e6f11608166f59c34aac7f6241790
Md. Mazharul Islam <mithu.cste.30@gmail.com>
2012-10-24 13:53:43 +0600

    updated the users controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -51,9 +51,41 @@ render_views
     end
 
     it 'should have the right title' do
-    	get :new
+    	post :create, :user => @attr 
     	response.should have_selector('title', :content => 'Sign up')
+    end
+
+    it "should rener the 'view' page" do
+      post :create, :user => @attr
+      response.should render_template('new')
+    end
+
+    it "should not create a user" do
+      lambda do
+        post :create, :user => @attr
+      end.should_not change(User, :count)
     end
   end
 
+  describe 'sucess' do
+    before(:each) do
+      @attr = {:name => 'sample user', :email => 'someone@example.com', 
+               :password => 'foobar',   :password_cofirmation => 'foobar'}
+      end
+    it 'should create a user' do
+      lambda do
+        post :create, :user => @attr
+      end.should change(User, :count).by(1)
+    end
+
+    it 'should redirect to the user show page' do
+      post :create, :user => @attr
+      response.should redirect_to(user_path(assigns(:user)))
+    end
+
+    it 'should have a welcome message' do
+      post :create, :user => @attr
+      flash[:success].should =~ /welcome to the sample app/i
+    end
+  end
 end
