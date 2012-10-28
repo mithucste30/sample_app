a1d98e96c411b7431106601436557924ce6bde27
Md. Mazharul Islam <mithu.cste.30@gmail.com>
2012-10-28 12:06:53 +0600

    completed signin and signout

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -87,5 +87,10 @@ render_views
       post :create, :user => @attr
       flash[:success].should =~ /welcome to the sample app/i
     end
+
+    it "should sign the user in" do
+      post :create, :user => @attr
+      controller.should be_signed_in
+    end
   end
 end
